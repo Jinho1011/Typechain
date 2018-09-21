a790d3d00057832db4eb67c5e8f092e7301ace09
전진호 <gino9940@gmail.com>
2018-09-21 18:55:20 +0900

    ts

---
+++ index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const sayHi = (name, age, gender) => {
+    console.log(`Hello ${name}(${age}), you are a ${gender}`);
+};
+sayHi("jinho", 18, "male");
+//# sourceMappingURL=index.js.map
---
+++ index.ts
@@ -0,0 +1,7 @@
+const sayHi = (name:string, age:number, gender:string):void => {
+    console.log(`Hello ${name}(${age}), you are a ${gender}`)
+};
+
+sayHi("jinho", 18, "male");
+
+export {};
